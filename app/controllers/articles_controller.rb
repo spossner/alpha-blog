70098a8121abecd98d75e420769d784006f4a494
Sebastian Possner <sebastian.possner@brands4friends.de>
2020-03-29 12:03:59 +0200

    add new, create, edit and update actions and views to maintain articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,32 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Successfully saved"
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Successfully updated"
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
 end
