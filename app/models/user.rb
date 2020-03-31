b7460ac63e5509f47705e24c2a5f4f4bde8efedb
Sebastian Possner <sebastian.possner@brands4friends.de>
2020-03-31 21:23:08 +0200

    add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\z/i }
+
+  has_secure_password
 end
