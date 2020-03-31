9be6298d92620372fbf52f4a1c3d5126949b1f0f
Sebastian Possner <sebastian.possner@brands4friends.de>
2020-03-31 10:00:06 +0200

    style the article overview page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -17,3 +16,7 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import "bootstrap"
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+})
