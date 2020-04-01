b18e877bc016e5965e0eb34108c5dc2d33c4f8fa
Sebastian Possner <sebastian.possner@brands4friends.de>
2020-04-01 16:38:15 +0200

    add update user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :find_user_by_id, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -13,7 +15,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    puts @user
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path # for now...
+    else
+      render :edit
+    end
+  end
+
   private
+  def find_user_by_id
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
