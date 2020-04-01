aba09f32150716f4b44b011e3cc3a892cb1460da
Sebastian Possner <sebastian.possner@brands4friends.de>
2020-04-01 21:53:46 +0200

    create show action and add gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  before_action :find_user_by_id, only: [:edit, :update]
+  before_action :find_user_by_id, only: [:show, :edit, :update]
+
+  def show
+  end
 
   def new
     @user = User.new
