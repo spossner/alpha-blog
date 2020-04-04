d6242d4023f60697ea85f1cafeb577ce91890dcd
Sebastian Possner <sebastian.possner@brands4friends.de>
2020-04-04 10:42:56 +0200

    fix to be ble to create new user w/o same user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :find_user_by_id, only: [:show, :edit, :update]
-  before_action :require_same_user, except: [:index, :show]
+  before_action :find_user_by_id, only: [:show, :edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
