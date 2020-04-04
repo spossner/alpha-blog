523d8ebad2f97767e807d1501377ded9a803b3a7
Sebastian Possner <sebastian.possner@brands4friends.de>
2020-04-04 10:49:44 +0200

    keep fresh user logged in by storing the id into the session; fixed the sign up button link on root home page to signup_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      session[:username] = user.username
       flash[:success] = "Login successfully"
       redirect_to user_path(user)
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Seppo Blog #{@user.username}"
       puts "redirect to index"
       redirect_to @user
